2749c5303056893a1d7d7af7a3fc1ea5a4df4833
SuryaTejaReddyMallidi <surya.mallidi4@gmail.com>
2023-10-05 17:00:39 +0530

    Final updated

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,10 +4,6 @@ import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 import { GiTeacher } from "react-icons/gi";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7361881045611bd9c78e0eb07a5f38c4c0426559
 const ServiceCard = ({ color, title, icon, subtitle }) => (
   <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
     <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
@@ -32,20 +28,11 @@ const Services = () => (
           Capstone 2023
         </h1>
         <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-<<<<<<< HEAD
           Together, we build, create, and drive our project forward. Together we shine.
         </p>
       </div>
 
       <div className="flex-1 flex flex-col justify-start items-left">
-=======
-          The best choice for buying and selling your crypto assets, with the
-          various super friendly services we offer
-        </p>
-      </div>
-
-      <div className="flex-1 flex flex-col justify-start items-center">
->>>>>>> 7361881045611bd9c78e0eb07a5f38c4c0426559
         <ServiceCard
           color="bg-[#8945F8]"
           title="Name : GULLIPALLI VISHWA"
@@ -65,11 +52,7 @@ const Services = () => (
           subtitle="Reg : 20BCE7457      "
         />
         <ServiceCard
-<<<<<<< HEAD
           color="bg-[#55BA28]"
-=======
-          color="bg-[#F84550]"
->>>>>>> 7361881045611bd9c78e0eb07a5f38c4c0426559
           title="Guided by : Dr. NIHAR RANJAN PRADHAN "
           icon={<GiTeacher fontSize={21} className="text-white" />}
           subtitle="      "
